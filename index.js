c4e5bd6082f6633283305f2edab4853fbc5f42b3
BrandyBecker <BrandyBeckerFSW@Gmail.com>
2020-01-16 12:01:28 -0500

    Initial SetUp

---
+++ index.js
@@ -0,0 +1,21 @@
+// Step Four: Build out a basic DogCard component, we should only concern ourselves with displaying the image at this point.
+
+
+// Step Three: talk about HTTP, requesting data from a server, and axios
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Once an HTTP request is made, Axios returns a promise that is either fulfilled or rejected, depending on the response from the backend service. To handle the result, you can use the then() method */
+
+/* If the promise is fulfilled, the first argument of then() will be called; if the promise is rejected, the second argument will be called. */
